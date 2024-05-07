3356836cc036ab0dcdece01b50424add91c5981f
Nicolas <nicolas.moyon@ynov.com>
2024-05-07 17:25:26 +0200

    push of geolocalisation system, working so far

--- servJS/app.js
+++ servJS/app.js
@@ -1,6 +1,7 @@
 // Import the Express.js module
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const axios = require('axios');
 
 // Create an instance of Express.js
 const app = express();
@@ -11,6 +12,34 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+
+app.get('/getAddress/:address', async (req, res) => {
+const address = req.params.address;
+const apiUrl = 'https://nominatim.openstreetmap.org/search';
+
+try{
+    const response = await axios.get(apiUrl,{
+        params: {
+            q:address,
+            format:'json'
+        }
+    });
+    const Suggestions = response.data.map(result => ({
+        address: result.display_name,
+        latitude: result.lat,
+        longitude: result.lon
+    }));
+
+    const Data ={
+        Suggestions: Suggestions,
+    }
+    res.json(Data);
+
+}catch(err){
+    console.error('Error:',err.message)
+    res.status(500).send({error :'Internal Server Error'});
+}
+})
 
 // Define the port number
 const port = 4000;
