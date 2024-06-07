8eac76d3ec21b6c4a8a4c7866ea023eff5d63ed9
Nicolas <nicolas.moyon@ynov.com>
2024-06-07 12:27:08 +0200

    udpating api to make sure it works, slowly but surely, and it's going well

--- servJS/backEnd/routes/apiRoutes.js
+++ servJS/backEnd/routes/apiRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { getAddressSuggestions } = require('../controllers/addressController');
 const { getCloseDeliveryPoint } = require('../controllers/deliveryPointController');
 const { toggleFavoriteItem, toggleCartItem } = require('../controllers/itemController');
+const {checkUserLogs, logoutUser, registerUser} = require("../controllers/authController");
 
 // Define a route to get address suggestions
 router.get('/getAddress/:address', getAddressSuggestions);
@@ -16,5 +17,14 @@ router.get('/setFav/:id', toggleFavoriteItem);
 
 // Route to add/remove an item to/from the cart
 router.get('/addToCart/:id', toggleCartItem);
+
+// Route to log user
+router.post('/Login', checkUserLogs);
+
+// Route to log out user
+router.post('/logoutUser', requireAuth,logoutUser)
+
+// Route to register user
+router.post('/registerUser', registerUser)
 
 module.exports = router;
