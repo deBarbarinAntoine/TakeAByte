05bdd50d7a8f01a183927d134f5bcc245b95ee5d
Nicolas13100 <nicolas.moyon2@gmail.com>
2024-06-16 23:40:23 +0200

    sales working for product

--- INSTALL/init.sql
+++ INSTALL/init.sql
@@ -7,3 +7,10 @@ VALUES
 (1, 5, NOW());
 
 UPDATE products SET quantity_stocked = 10;
+
+UPDATE sales SET reduction_percentage = 20 WHERE product_id = 1;
+UPDATE sales SET reduction_percentage = 12 WHERE product_id = 2;
+UPDATE sales SET reduction_percentage = 50 WHERE product_id = 17;
+UPDATE sales SET reduction_percentage = 75 WHERE product_id = 100;
+UPDATE sales SET reduction_percentage = 2 WHERE product_id = 86;
+UPDATE sales SET reduction_percentage = 50 WHERE product_id = 25;
