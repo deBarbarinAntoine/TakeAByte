df53caa283dfadb876b3f1a3e092c3ba070942bd
Nicolas13100 <nicolas.moyon2@gmail.com>
2024-07-01 15:33:34 +0200

    init a fake user for anonyms

--- INSTALL/init.sql
+++ INSTALL/init.sql
@@ -29,3 +29,8 @@ VALUES (52, 25.00, NOW(), DATE_ADD(NOW(), INTERVAL 1 MONTH));
 INSERT INTO sales (product_id, reduction_percentage, start_date, end_date)
 VALUES (80, 18.00, NOW(), DATE_ADD(NOW(), INTERVAL 1 MONTH));
 
+INSERT INTO users (
+    username, email, password_hash, name, lastname, province, country, city, zip_code, street_name, street_number, address_complements, is_mod
+) VALUES ( 'user_zero', 'user_zero@example.com', 'hashed_password', 'First', 'Last', 'Province', 'CNY', 'City', '12345', 'Street', 123, 'Apt 1', 0
+         );
+UPDATE `users` SET `user_id` = '0' WHERE `users`.`username` = 'user_zero'
